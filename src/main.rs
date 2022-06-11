edc70c8df58740ba5dc9c4423ce258e45f391a2c
Darren Barklie <darrenbarklie@gmail.com>
2022-06-11 23:43:10 +0100

    Config errors, integrate write command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+use structopt::StructOpt;
+
+/// A CLI for the growing and curation of a digital garden
+/// by https://daz.dev via https://egghead.io
+#[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
+struct Opt {
+  #[structopt(subcommand)]
+  cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+  /// Write something in your digital garden
+  ///
+  /// This command will open your $EDITOR, wait for you
+  /// to write something, and then save the file to
+  /// your digital garden
+  Write {
+    #[structopt(short, long)]
+    title: Option<String>,
+  },
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let opt = Opt::from_args();
+    dbg!(opt);
+    todo!();
 }
